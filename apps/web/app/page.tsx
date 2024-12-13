4438f47722834c267cf63f39c224fe75a7185fa2
Kyo Terada <kyo.terada@sellca-sellcar.com>
2024-12-13 16:19:27 +0900

    test ci

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,9 @@
 import { Button } from "@repo/ui/button";
 
 export default function Home() {
-	return <Button appName="web">Click</Button>;
+	return (
+		<div className="p-4 bg-yellow-500">
+			<Button appName="web">Click</Button>
+		</div>
+	);
 }
