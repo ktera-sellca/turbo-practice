e9dd2fc311036e1392acfd07c9975bedb0246c9b
Kyo Terada <kyo.terada@sellca-sellcar.com>
2024-12-13 17:34:07 +0900

    Update page.tsx

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -3,9 +3,6 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-
-
-	
 	return (
 		<div className={styles.page}>
 			<main className={styles.main}>
