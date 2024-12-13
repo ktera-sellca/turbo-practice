384151b7a74edb6db2daf099659d4cba8e31a5fc
Kyo Terada <kyo.terada@sellca-sellcar.com>
2024-12-13 17:33:02 +0900

    pull req test from t-2 (#2)
    * pull req test from t-2
    
    * test ci
    
    * test ci v4
    
    * test ci v5
    
    * test paths
    
    * empty commit
    
    * why?

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,8 +3,9 @@ import { Button } from "@repo/ui/button";
 export default function Home() {
 	return (
 		<div className="p-4 bg-yellow-500">
-			<Button appName="web">Click</Button>
-			<Button appName="web">Click</Button>
+			<Button appName="web">Click1</Button>
+			<Button appName="web">Click2</Button>
+			<Button appName="web">Click3</Button>
 		</div>
 	);
 }
